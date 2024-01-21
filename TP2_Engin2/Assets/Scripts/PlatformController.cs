1f53611b5407ae677a57f4b8bf835d20471c8a58
philplaisir <p.montplaisir-m@hotmail.com>
2024-01-21 15:57:58 -0500

    Still working on it, refactored code so the platform reacts accordingly to the input received depending on the position of the camera around the platform

--- TP2_Engin2/Assets/Scripts/PlatformController.cs
+++ TP2_Engin2/Assets/Scripts/PlatformController.cs
@@ -13,8 +13,34 @@ public class PlatformController : MonoBehaviour
     [SerializeField]
     private float m_pivotRadius = 10.0f;
 
+    private Vector3 m_localForward = Vector3.zero;
+    private Vector3 m_localRight = Vector3.zero;
+    private float m_rotationX = 0.0f;
+    private float m_rotationZ = 0.0f;
+
+
+
+    // À delete
+    public Camera camera1;
+    public Camera camera2;
+    public Camera camera3;
+    public Camera camera4;
+
+    private void Start()
+    {
+        // À delete
+        camera1.enabled = true;
+        camera2.enabled = false;
+        camera3.enabled = false;
+        camera4.enabled = false;
+    }
+
     private void Update()
-    {        
+    {
+        CameraChanges();
+
+        Camera activeCamera = GetActiveCamera();
+
         float inputXPositive = 0.0f;
         float inputXNegative = 0.0f;
         float inputZPositive = 0.0f;
@@ -43,23 +69,89 @@ public class PlatformController : MonoBehaviour
         if (inputXPositive == 0 && inputXNegative == 0 && inputZPositive == 0 && inputZNegative == 0)
             return;
 
-        float rotationX = (inputXPositive + inputXNegative) * m_rotationSpeed * Time.deltaTime;
-        float rotationZ = (inputZPositive + inputZNegative) * m_rotationSpeed * Time.deltaTime;
-                
+        m_localForward = activeCamera.transform.TransformDirection(Vector3.forward);
+        m_localRight = activeCamera.transform.TransformDirection(Vector3.right);
+
+        m_rotationX = (inputXPositive + inputXNegative) * m_rotationSpeed * Time.deltaTime;
+        m_rotationZ = (inputZPositive + inputZNegative) * m_rotationSpeed * Time.deltaTime;
+
+        ApplyRotate(m_previewObject);
+
+        if (CalculateAngleFromPivot() >= m_angleLimit)
+            return;
+
+        ApplyRotate(gameObject);
+    }
+
+    private float CalculateAngleFromPivot()
+    {
+        Vector3 pivotToObjectPreviewDir = m_pivot.position - m_previewObject.transform.position;
+        float previewObjectToPivotDirAngle = Vector3.Angle(-Vector3.up, pivotToObjectPreviewDir);
+        // Retirer le commentaire
+        Debug.Log("Angle with preview object " + previewObjectToPivotDirAngle);
+        return previewObjectToPivotDirAngle;
+    }
+
+    private void ApplyRotate(GameObject gameObject)
+    {
+        if (gameObject == null) return;
+
         m_previewObject.transform.position = transform.position;
         m_previewObject.transform.rotation = transform.rotation;
 
-        m_previewObject.transform.position = m_pivot.position - (-transform.up * m_pivotRadius);
-        m_previewObject.transform.RotateAround(m_pivot.position, Vector3.right, rotationX);
-        m_previewObject.transform.RotateAround(m_pivot.position, Vector3.forward, rotationZ);
+        gameObject.transform.position = m_pivot.position - (-transform.up * m_pivotRadius);
+        gameObject.transform.RotateAround(m_pivot.position, m_localRight, m_rotationX);
+        gameObject.transform.RotateAround(m_pivot.position, m_localForward, m_rotationZ);
+    }
 
-        Vector3 pivotToObjectPreviewDir = m_pivot.position - m_previewObject.transform.position;
-        float previewObjectToPivotDirAngle = Vector3.Angle(-Vector3.up, pivotToObjectPreviewDir);
+    private void CameraChanges()
+    {
+        // À delete
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchToCamera(camera1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SwitchToCamera(camera2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SwitchToCamera(camera3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SwitchToCamera(camera4);
+        }
+    }
 
-        if (previewObjectToPivotDirAngle >= m_angleLimit)
-            return;
+    void SwitchToCamera(Camera targetCamera)
+    {   
+        // À delete
+        camera1.enabled = false;
+        camera2.enabled = false;
+        camera3.enabled = false;
+        camera4.enabled = false;
 
-        transform.position = m_pivot.position - (-transform.up * m_pivotRadius);
-        transform.RotateAround(m_pivot.position, Vector3.right, rotationX);
-        transform.RotateAround(m_pivot.position, Vector3.forward, rotationZ);
-    }}
+        targetCamera.enabled = true;
+    }
+
+    Camera GetActiveCamera()
+    {
+        // À delete
+        if (camera1.enabled)
+            return camera1;
+        else if (camera2.enabled)
+            return camera2;
+        else if (camera3.enabled)
+            return camera3;
+        else if (camera4.enabled)
+            return camera4;
+        else
+            return null;
+    }
+
+
+}
+
+
