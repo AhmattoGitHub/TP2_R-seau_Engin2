d117bf7291b44d775bacb4c6e752a5d46e352585
philplaisir <p.montplaisir-m@hotmail.com>
2024-02-17 23:40:53 -0500

    Added UI Win/Lose handling (the canvases may need some work), Added particle effect to bombs explosion, Added jewel asset on top of level, added platform disappears to some platforms

--- TP2_Engin2/Assets/Scripts/Level/Rotate.cs
+++ TP2_Engin2/Assets/Scripts/Level/Rotate.cs
@@ -1,16 +1,14 @@
 using Mirror;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotate : NetworkBehaviour
 {
     [SerializeField] private float m_rotationSpeed;
-    private float m_rotation = 0;
-
+    private float m_rotation = 0;    
+    
     void FixedUpdate()
     {
-        //transform.RotateAround(transform.position, Vector3.up, m_rotationSpeed);
+        //transform.RotateAround(transform.position, Vector3.up, m_rotationSpeed);       
 
         transform.localRotation = Quaternion.Euler(0, m_rotation, 0);
         m_rotation += m_rotationSpeed;
